f61447d4d4c4722489887ef95bc5f2a5dfa6662e
rupert@it031740 <rupertcollins@gmail.com>
2018-11-29 16:24:47 +0000

    revamped the code, removed gbif stuff
    Former-commit-id: c1133b46799fadd599408fa5ab2f38295b07d041

--- scripts/species-list.R
+++ scripts/species-list.R
@@ -2,65 +2,23 @@
 
 # load libs
 require("tidyverse")
-require("rgbif")
 require("rfishbase")
-require("taxize")
-require("stringr")
-require("parallel")
 
-# script makes a list of all UK marine and freshwater species of fishes (inc sharks and rays)
-# uses (1) gbif records; (2) fishbase; and (3) WFD transitional species list.
-
-
-## Get list from GBIF
-# Don't need to run once already done (done 20/12/17)
-
-# get taxa names for sharks and fish
-keyfish <- name_backbone(name='Actinopterygii')$classKey
-keyshark <- name_backbone(name='Elasmobranchii')$classKey
-
-# get max counts for records (only preserved specimen records with locality data, i.e. with vouchers) 
-maxfish <- occ_count(taxonKey=keyfish, country="GB", georeferenced=TRUE, basisOfRecord="PRESERVED_SPECIMEN")
-maxshark <- occ_count(taxonKey=keyshark, country="GB", georeferenced=TRUE, basisOfRecord="PRESERVED_SPECIMEN")
-
-# download the records (only preserved specimen records with locality data, i.e. with vouchers) 
-fishdb <- occ_search(taxonKey=keyfish, country="GB", hasCoordinate=TRUE, basisOfRecord="PRESERVED_SPECIMEN", return="data", fields="all", limit=maxfish)
-sharkdb <- occ_search(taxonKey=keyshark, country="GB", hasCoordinate=TRUE, basisOfRecord="PRESERVED_SPECIMEN", return="data", fields="all", limit=maxshark)
-
-# dataset dimensions
-dim(sharkdb)
-dim(fishdb)
-
-# merge the dbs
-gbif.db <- dplyr::full_join(fishdb, sharkdb)
-
-# removed those with no species level ID and those with non-euro locations (overseas territories and mistakes) and extant from palao collections
-gbif.db <- gbif.db %>% filter(taxonRank == "SPECIES") %>% filter(!(decimalLatitude < 40 | decimalLongitude < -30)) %>% filter(collectionCode!="PAL")
-
-# get the names and write out to temp 
-gbif.sp <- gbif.db %>% mutate(species=paste(genus, specificEpithet)) %>% select(species) %>% distinct(species, .keep_all=TRUE) %>% arrange(species)
-#write_csv(gbif.sp, path="../temp/gbif_tmp.csv")
-
-
-## Once GBIF is done add to list from FishBase and the WFD 
-# these both need to be done BY HAND from website and pdf
-
-# load up the combined species list
-uk.list <- read_csv(file="../data/uk-species-list.csv")
+# load up the combined species list (generated 20/12/2017)
+uk.list <- read_csv(file="../species/uk-species-list.csv")
 
 # get uniques
 uniqs <- uk.list %>% filter(source!="common") %>% select(species) %>% distinct(species)
 
 # get valid names using fishbase and remove mispellings and synonyms
 # takes a few minutes
-val.uniqs <- validate_names(species_list=uniqs$species)
+val.uniqs <- validate_names(species_list=uniqs$species,server="fishbase")
 val.uniqs <- unique(val.uniqs)
-# check for probs
-warnings()
 
 # check
-setdiff(uniqs$species, val.uniqs)
-setdiff(val.uniqs, uniqs$species)
+setdiff(uniqs$species, val.uniqs)# in orig list, not in validated
+setdiff(val.uniqs, uniqs$species)# in validated, not in orig
+rejected.names <- c(setdiff(uniqs$species, val.uniqs),setdiff(val.uniqs, uniqs$species))
 
 # load up the fishbase data
 data(fishbase)
@@ -89,20 +47,31 @@ tidy.uk.list$commonSpecies[match(common.sp$species, tidy.uk.list$sciName)] <- TR
 glimpse(tidy.uk.list)
 tidy.uk.list %>% print(n=Inf)
 
-# now get synonyms for all species
-fish.syns <- mclapply(tidy.uk.list$specCode, FUN=function(x) rfishbase::synonyms(x,limit=100), mc.cores=8)
+# function to retrieve synonyms for all species
+grab_synonyms <- function(synonyms,code){
+syns <- synonyms %>% filter(SpecCode==code)
+return(syns)
+}
 
-# join and clean
-syns.table <- bind_rows(fish.syns) %>% 
-    mutate(genSpp=paste(SynGenus, SynSpecies), sciName=paste(str_split_fixed(genSpp, " ", 3)[,1], str_split_fixed(genSpp, " ", 3)[,2]), synonym=rep(TRUE)) %>% 
+# run function for all spp
+fish.syns <-  bind_rows(lapply(tidy.uk.list$specCode, function(x) grab_synonyms(synonyms=synonyms.all,code=x)))
+
+# subset the synonyms and the subspecific names
+fish.syns <- bind_rows((fish.syns %>% filter(Status=="synonym")), (fish.syns %>% filter(Status=="accepted name" & TaxonLevel=="Nominotypical")))
+
+# tidy and filter
+syns.table <- fish.syns %>% 
+    rename(specCode=SpecCode,sciName=synonym) %>% 
+    mutate(synonym=rep(TRUE)) %>% 
     distinct(sciName, .keep_all=TRUE) %>% 
-    filter(!sciName %in% tidy.uk.list$sciName)
+    filter(!sciName %in% tidy.uk.list$sciName) %>%
+    select(specCode,synonym,sciName)
 
-# reduce
-syns.table <- syns.table %>% select(SpecCode,SynGenus,SynSpecies,sciName,synonym) %>% rename(specCode=SpecCode,genus=SynGenus,species=SynSpecies)
-
-# merge with all uk species
+# merge with all uk species 
 tidy.uk.list.syns <- bind_rows(tidy.uk.list,syns.table)
 
+# add the rejected names from the earlier validation
+tidy.uk.list.syns <- bind_rows(tidy.uk.list.syns,tibble(sciName=setdiff(rejected.names,tidy.uk.list.syns$sciName),synonym=rep(TRUE)))
+
 # write out
-write_csv(tidy.uk.list.syns, path="../data/uk-species-table.csv")
+write_csv(tidy.uk.list.syns, path="../species/uk-species-table.csv")
