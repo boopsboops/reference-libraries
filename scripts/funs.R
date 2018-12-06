0a611a1265a0963d736c7f4fee803f1022a3dd15
rupert@it031740 <rupertcollins@gmail.com>
2018-12-06 11:31:57 +0000

    moved hmmer and libs into funs.R
    Former-commit-id: 2600620a481531fa3c32d57a41c167a15710cfe0

--- scripts/funs.R
+++ scripts/funs.R
@@ -1,6 +1,32 @@
 #!/usr/bin/env Rscript
 library("stringr")
 library("parallel")
+library("ape")
+library("tidyverse")
+library("magrittr")
+library("rfishbase")
+library("traits")
+library("rentrez")
+library("bold")
+library("rgbif")
+library("phangorn")
+library("ips")
+library("stringdist")
+
+## NOTE ##
+# until changes come through to CRAN, need to run dev version of traits and install with sudo R in the terminal 
+# either 'install_github("ropensci/traits")' or for root lib locations:
+# sudo R
+# library(devtools); withr::with_libpaths(new = "/usr/local/lib/R/site-library/", install_github("ropensci/traits"))
+
+# print the R session info
+sink("../temp/RsessionInfo.txt")
+print(sessionInfo())
+sink()
+
+
+# function for making fasta files from tables
+source("https://raw.githubusercontent.com/legalLab/protocols-scripts/master/scripts/tab2fas.R")
 
 
 # collapses haplotypes (from a dataframe format to a dataframe format)
@@ -116,4 +142,34 @@ read_GenBank <- function (access.nb, seq.names = access.nb, species.names = TRUE
     if (gene.names) 
         warning("you used 'gene.names = TRUE': this option is obsolete; please update your code.")
     res
+}
+
+
+# R script to run a hidden markov model on a sequence
+# need to have "hmmer" and "biosquid" installed 
+# if not, run 'sudo apt install hmmer biosquid'
+# also needs the ape package
+# requires a tempfile directory (e.g. "../temp")
+# requires an infile in fasta format to be in the same dir as the tempfiles (e.g. "myfile.fas")
+# requires the name of the hmm you want to use (e.g. "12s.miya.noprimers.hmm")
+# requires a prefix for the hmmer output (e.g. "analysis1")
+# assumes the hidden markov model is located in ../hmms 
+# returns a DNAbin object of the sequences matched by hmmer 
+
+run_hmmer3 <- function(dir, infile, hmm, prefix, evalue, coords){#
+    string.hmmer <- paste0("nhmmer -E ", evalue, " --incE ", evalue, " --dfamtblout ", dir, "/", prefix, ".hmmer.tbl ", "../hmms/", prefix, ".hmm ", dir, "/", infile)
+    system(command=string.hmmer, ignore.stdout=TRUE)
+    hmm.tbl <- read_delim(file=paste0(dir, "/", prefix, ".hmmer.tbl"), delim=" ", col_names=FALSE, trim_ws=TRUE, progress=FALSE, comment="#", col_types=cols(), guess_max=100000)
+    names(hmm.tbl) <- c("targetName","acc","queryName","bits","eValue","bias","hmmStart","hmmEnd","strand","aliStart","aliEnd","envStart","envEnd","sqLen","descriptionTarget")
+    hmm.tbl %<>% filter(strand=="+") %>% distinct(targetName, .keep_all=TRUE) %>% mutate(coords=paste(envStart,envEnd,sep=":"))
+    mtdna <- read.FASTA(file=paste0(dir,"/",infile))
+    mtdna.sub <- as.character(mtdna[match(hmm.tbl$targetName,names(mtdna))])
+    if(coords=="env"){
+    mtdna.sub.coords <- as.DNAbin(mapply(function(x,y,z) x[y:z], mtdna.sub, hmm.tbl$envStart, hmm.tbl$envEnd, SIMPLIFY=TRUE, USE.NAMES=TRUE))
+    } else if(coords=="ali"){
+    mtdna.sub.coords <- as.DNAbin(mapply(function(x,y,z) x[y:z], mtdna.sub, hmm.tbl$aliStart, hmm.tbl$aliEnd, SIMPLIFY=TRUE, USE.NAMES=TRUE))
+    } else {
+    stop("Please provide 'env' or 'ali' as arguments to coords")
+    }
+    return(mtdna.sub.coords)
 }
