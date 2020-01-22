911e6359eccd2c48f31a0818335fa20b3c3600e7
rupert@it031740 <rupertcollins@gmail.com>
2020-01-22 23:01:55 +0000

    rewritten to use fb tables better

--- scripts/species-list-clean.R
+++ scripts/species-list-clean.R
@@ -13,102 +13,75 @@ fishbase.species <- rfishbase::species(server="fishbase")
 fishbase.taxonomy <- rfishbase::load_taxa(server="fishbase")
 fishbase.synonyms <- rfishbase::synonyms(server="fishbase")
 
-# get uniques
-uniqs <- uk.list %>% filter(source!="common") %>% select(species) %>% distinct(species)
+# removed subspecies and get uniques
+uniqs <- uk.list %>% 
+    filter(source!="common") %>% 
+    mutate(speciesName=paste(str_split_fixed(species," ",3)[,1],str_split_fixed(species," ",3)[,2])) %>% 
+    distinct(speciesName)
 
 # annotate with SpecCode
-fishbase.synonyms.acc <- fishbase.synonyms %>% filter(Status=="accepted name")
-fishbase.synonyms.syn <- fishbase.synonyms %>% filter(Status=="synonym" | Status=="Synonym")
+fishbase.synonyms.acc <- fishbase.synonyms %>% mutate(TaxonLevel=str_replace_all(TaxonLevel,"^species","Species")) %>% filter(Status=="accepted name" & TaxonLevel=="Species")
+fishbase.synonyms.syn <- fishbase.synonyms %>% mutate(Status=str_replace_all(Status,"Synonym","synonym")) %>% filter(Status=="synonym")
+# fishbase.synonyms.syn %>% pull(Status) %>% unique
 
 # annotate with synonym status
-uniqs.ann <- as_tibble(uniqs) %>% mutate(status=pull(fishbase.synonyms.acc,Status)[match(species,pull(fishbase.synonyms.acc,synonym))]) %>% 
-    mutate(status=if_else(is.na(status),pull(fishbase.synonyms.syn,Status)[match(species,pull(fishbase.synonyms.syn,synonym))],status)) %>% 
-    mutate(status=str_replace_all(status,"Synonym","synonym"))
+uniqs.ann <- uniqs %>% 
+    mutate(status=pull(fishbase.synonyms.acc,Status)[match(speciesName,pull(fishbase.synonyms.acc,synonym))]) %>% 
+    mutate(status=if_else(is.na(status),pull(fishbase.synonyms.syn,Status)[match(speciesName,pull(fishbase.synonyms.syn,synonym))],status))
 
 # print those without syns
-print("Synonyms/accepted names for the following species could not be found in FishBase. They have been dropped.")
-uniqs.ann %>% filter(is.na(status)) %>% pull(species)
+drops <- uniqs.ann %>% filter(is.na(status)) %>% pull(speciesName) %>% paste(collapse=", ")
+writeLines(paste0("Synonyms/accepted names for the following species could not be found in FishBase, and have been dropped: ", "\n", drops))
 
 # drop those without status
 uniqs.ann %<>% filter(!is.na(status))
 
 # annotate with SpecCode
-uniqs.ann %<>% mutate(fbSpecCode=pull(fishbase.synonyms.acc,SpecCode)[match(species,pull(fishbase.synonyms.acc,synonym))]) %>% 
-    mutate(fbSpecCode=if_else(is.na(fbSpecCode),pull(fishbase.synonyms.syn,SpecCode)[match(species,pull(fishbase.synonyms.syn,synonym))],fbSpecCode))
+uniqs.ann %<>% mutate(fbSpecCode=pull(fishbase.synonyms.acc,SpecCode)[match(speciesName,pull(fishbase.synonyms.acc,synonym))]) %>% 
+    mutate(fbSpecCode=if_else(is.na(fbSpecCode),pull(fishbase.synonyms.syn,SpecCode)[match(speciesName,pull(fishbase.synonyms.syn,synonym))],fbSpecCode))
 
-# get the accepted name
-uniqs.ann %>% mutate(validName=pull(fishbase.species,Species)[match(fbSpecCode,pull(fishbase.species,SpecCode))])
-    #mutate(validName=apply(str_split_fixed(validName, " ", 3)[,1:2], 1, paste, collapse=" ")) %>% 
-#filter(species!=validName) %>% print(n=Inf)
+# get the accepted name 
+uniqs.ann %<>% mutate(validName=pull(fishbase.synonyms.acc,synonym)[match(fbSpecCode,pull(fishbase.synonyms.acc,SpecCode))])
 
-fishbase.synonyms 
+# get synonyms
+uniqs.codes <- uniqs.ann %>% pull(fbSpecCode) %>% unique()
 
+# grab from FB - remove ones we already have, remove missapplied names and ambig synonyms, clean up any non-alphabetical chars, add valid name
+fishbase.synonyms.matched <- fishbase.synonyms %>% 
+    mutate(Status=str_replace_all(Status,"Synonym","synonym")) %>% 
+    filter(Status=="synonym") %>%
+    filter(SpecCode %in% uniqs.codes) %>% 
+    filter(!synonym %in% pull(uniqs.ann,speciesName)) %>% 
+    filter(!str_detect(synonym,"[^a-zA-Z\\d\\s:]")) %>% 
+    mutate(validName=pull(fishbase.synonyms.acc,synonym)[match(SpecCode,pull(fishbase.synonyms.acc,SpecCode))]) %>%
+    select(synonym,Status,SpecCode,validName) %>% 
+    rename(speciesName=synonym,status=Status,fbSpecCode=SpecCode) 
 
-# get valid names using fishbase and remove mispellings and synonyms
-# takes a few minutes
-val.uniqs <- validate_names(species_list=uniqs$species,server="fishbase")
-val.uniqs <- unique(val.uniqs)
+# join with our data
+spps.with.synonyms <- bind_rows(uniqs.ann,fishbase.synonyms.matched)
 
-# check
-setdiff(uniqs$species, val.uniqs)# in orig list, not in validated
-setdiff(val.uniqs, uniqs$species)# in validated, not in orig
-rejected.names <- c(setdiff(uniqs$species, val.uniqs),setdiff(val.uniqs, uniqs$species))
+# keep unique rows
+spps.with.synonyms %<>% distinct()
 
-# load up the fishbase data
-data(fishbase)
+# add taxonomy and common names
+spps.with.synonyms %<>% mutate(class=pull(fishbase.taxonomy,Class)[match(fbSpecCode,pull(fishbase.taxonomy,SpecCode))],
+    class=pull(fishbase.taxonomy,Class)[match(fbSpecCode,pull(fishbase.taxonomy,SpecCode))],
+    order=pull(fishbase.taxonomy,Order)[match(fbSpecCode,pull(fishbase.taxonomy,SpecCode))],
+    family=pull(fishbase.taxonomy,Family)[match(fbSpecCode,pull(fishbase.taxonomy,SpecCode))],
+    genus=pull(fishbase.taxonomy,Genus)[match(fbSpecCode,pull(fishbase.taxonomy,SpecCode))],
+    commonName=pull(fishbase.species,FBname)[match(fbSpecCode,pull(fishbase.species,SpecCode))])
 
-# make a sciname to match ours 
-gen.sp <- paste(fishbase$Genus, fishbase$Species)
+# make common spp table
+common.spp <- uk.list %>% 
+    filter(source=="common") %>% 
+    mutate(fbSpecCode=pull(fishbase.synonyms.acc,SpecCode)[match(species,pull(fishbase.synonyms.acc,synonym))]) %>% 
+    mutate(fbSpecCode=if_else(is.na(fbSpecCode),pull(fishbase.synonyms.syn,SpecCode)[match(species,pull(fishbase.synonyms.syn,synonym))],fbSpecCode)) %>%
+    mutate(validName=pull(fishbase.species,Species)[match(fbSpecCode,pull(fishbase.species,SpecCode))])
 
-# match positions
-fb.red <- fishbase[match(val.uniqs, gen.sp),]
-
-# choose column want to keep and make new sciname, also sort, and tidy up 
-# also edits the subspecies out 
-tidy.uk.list <- fb.red %>% #
-    select(Class, Order, Family, Genus, Species, SpecCode, FBname) %>% #
-    mutate(sciName=paste(Genus, Species), commonSpecies=rep(FALSE), synonym=rep(FALSE), sciName=paste(str_split_fixed(sciName, " ", 3)[,1],str_split_fixed(sciName, " ", 3)[,2])) %>% #
-    arrange(Class, Order, Family, Genus, Species) %>% #
-    rename(class=Class, order=Order, family=Family, genus=Genus, species=Species, commonName=FBname, specCode=SpecCode) %>%
-    distinct(sciName, .keep_all=TRUE)
-
-# add the common species field
-common.sp <- uk.list %>% filter(source=="common")
-common.sp %>% print(n=Inf)
-tidy.uk.list$commonSpecies[match(common.sp$species, tidy.uk.list$sciName)] <- TRUE
-
-# check
-glimpse(tidy.uk.list)
-tidy.uk.list %>% print(n=Inf)
-
-# function to retrieve synonyms for all species
-grab_synonyms <- function(synonyms,code){
-syns <- synonyms %>% filter(SpecCode==code)
-return(syns)
-}
-
-# get all synonyms from rfishbase
-synonyms.all <- synonyms(server="fishbase")
-
-# run function for all spp
-fish.syns <-  bind_rows(lapply(tidy.uk.list$specCode, function(x) grab_synonyms(synonyms=synonyms.all,code=x)))
-
-# subset the synonyms and the subspecific names
-fish.syns <- bind_rows((fish.syns %>% filter(Status!="accepted name")), (fish.syns %>% filter(Status=="accepted name" & TaxonLevel=="Nominotypical")))
-
-# tidy and filter
-syns.table <- fish.syns %>% 
-    rename(specCode=SpecCode,sciName=synonym) %>% 
-    mutate(synonym=rep(TRUE)) %>% 
-    distinct(sciName, .keep_all=TRUE) %>% 
-    filter(!sciName %in% tidy.uk.list$sciName) %>%
-    select(specCode,synonym,sciName)
-
-# merge with all uk species 
-tidy.uk.list.syns <- bind_rows(tidy.uk.list,syns.table)
-
-# add the rejected names from the earlier validation
-tidy.uk.list.syns <- bind_rows(tidy.uk.list.syns,tibble(sciName=setdiff(rejected.names,tidy.uk.list.syns$sciName),synonym=rep(TRUE)))
+# arrange and clean
+spps.with.synonyms %<>% mutate(commonSpecies=if_else(fbSpecCode %in% pull(common.spp,fbSpecCode),TRUE,FALSE)) %>% 
+    select(speciesName,status,fbSpecCode,validName,class,order,family,genus,commonName,commonSpecies) %>% 
+    arrange(class,order,family,genus,validName)
 
 # write out
-write_csv(tidy.uk.list.syns, path="../species/uk-species-table.csv")
+write_csv(spps.with.synonyms, path="../species/uk-species-table.csv")
